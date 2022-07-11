49d5b85bde0004bb70dcaa364e87037f37c196cd
2022-DEV1-045 <>
2022-07-11 22:14:26 +0530

    refactor: added ShoppingBasket class and ShoppingBasketItem for better code

---
+++ src/main/java/com/bnpp/kata/developmentbooks/ShoppingBasket.java
@@ -0,0 +1,7 @@
+package com.bnpp.kata.developmentbooks;
+
+import java.util.List;
+
+public class ShoppingBasket {
+	private List<ShoppingBasketItem> basketItems;
+}
---
+++ src/main/java/com/bnpp/kata/developmentbooks/ShoppingBasketItem.java
@@ -0,0 +1,14 @@
+package com.bnpp.kata.developmentbooks;
+
+public class ShoppingBasketItem {
+	private Book book;
+	private int bookQty;
+
+	public Book getBook() {
+		return book;
+	}
+
+	public int getQuantity() {
+		return bookQty;
+	}
+}
